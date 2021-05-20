bf7fa59e11f256690ad79f2d004ab0ead3f5fc75
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-20 11:26:43 +0200

    :sparkles: Add User Routes

---
+++ src/routes/user.route.js
@@ -0,0 +1,33 @@
+import express from 'express';
+
+import {
+  createUser,
+  getUsers,
+  getUser,
+  updateUser,
+  deleteUser
+} from '../controllers/user.controller';
+import auth from '../middlewares/auth';
+import validate from '../middlewares/validate';
+import {
+  createUserValidate,
+  getUsersValidate,
+  getUserValidate,
+  updateUserValidate,
+  deleteUserValidate
+} from '../validations/user.validation';
+
+const router = express.Router();
+
+router
+  .route('/')
+  .post(auth('admin'), validate(createUserValidate), createUser)
+  .get(validate(getUsersValidate), getUsers);
+
+router
+  .route('/:userId')
+  .get(validate(getUserValidate), getUser)
+  .patch(auth('admin'), validate(updateUserValidate), updateUser)
+  .delete(auth('admin'), validate(deleteUserValidate), deleteUser);
+
+export default router;
