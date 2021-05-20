67821fb4363949b26ddb75f81b1a105dff6407d1
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-20 11:09:04 +0200

    :wrench: Add DB Configs

---
+++ src/config/db.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+import config from './config';
+import logger from './logger';
+
+/**
+ * Connect To Database
+ */
+const connectDB = async () => {
+  const DB = config.db.url.replace('<PASSWORD>', config.db.password);
+
+  mongoose.set('autoIndex', true);
+
+  const con = await mongoose.connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    autoIndex: true
+  });
+
+  logger.info(`MongoDB Connected: ${con.connection.host}.`);
+
+  mongoose.connection.on('connecting', () => {
+    logger.info('Connecting to Database');
+  });
+
+  mongoose.connection.on('connected', () => {
+    logger.info('Mongoose Connected to Database');
+  });
+
+  mongoose.connection.on('error', (err) => {
+    logger.error(err.message);
+  });
+
+  mongoose.connection.on('disconnected', () => {
+    logger.info('Mongoose Connection is Disconnected.');
+  });
+
+  process.on('SIGINT', async () => {
+    await mongoose.connection.close();
+    process.exit(0);
+  });
+};
+
+export default connectDB;
