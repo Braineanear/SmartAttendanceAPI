60b335c1ceebf8db1e02d0152a5bb6c48fc05ca7
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-20 11:23:20 +0200

    :sparkles: Add Models

---
+++ src/models/index.js
@@ -0,0 +1,8 @@
+import Token from './token.model';
+import User from './user.model';
+import Group from './group.model';
+import Department from './department.model';
+import Course from './course.model';
+import Attendance from './attendance.model';
+
+export { Token, User, Group, Department, Course, Attendance };
