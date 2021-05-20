bb9cf1f54f01ba7181129593c3075da3eb635eee
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-20 11:27:22 +0200

    :sparkles: Add Course Routes

---
+++ src/routes/course.route.js
@@ -0,0 +1,23 @@
+// External Packages
+import express from 'express';
+
+// Middlewares
+import auth from '../middlewares/auth';
+
+// Controllers
+import { courseController } from '../controllers/index';
+
+const { createCourse, getAllCourses, getCourse, updateCourse, deleteCourse } =
+  courseController;
+
+const router = express.Router();
+
+router.route('/').get(getAllCourses).post(auth('admin'), createCourse);
+
+router
+  .route('/')
+  .get(getCourse)
+  .patch(auth('admin'), updateCourse)
+  .delete(auth('admin'), deleteCourse);
+
+export default router;
