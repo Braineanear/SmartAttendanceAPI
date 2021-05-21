7335a08785b400b50f344a007ea696ac86a8a0f3
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-21 17:03:25 +0200

    :sparkles: Add toJSON plugin

--- src/models/attendance.model.js
+++ src/models/attendance.model.js
@@ -1,16 +1,36 @@
 import moment from 'moment';
 import mongoose from 'mongoose';
+import toJSON from './plugins/toJSON.plugin';
 
 const attendanceSchema = mongoose.Schema(
   {
     date: {
-      type: Date,
-      default: moment()
+      type: String,
+      default: moment().format('l')
     },
-    students: [Array]
+    student: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true
+    },
+    course: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Course',
+      required: true
+    },
+    courseStartsAt: {
+      type: String,
+      required: true
+    },
+    courseEndsAt: {
+      type: String,
+      required: true
+    }
   },
   { timestamps: true }
 );
+
+attendanceSchema.plugin(toJSON);
 
 const Attendance = mongoose.model('Attendance', attendanceSchema);
 
