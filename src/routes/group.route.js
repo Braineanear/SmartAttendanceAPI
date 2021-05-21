bb056f2668c1cc2dea4b467abb319e092f05b61b
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-21 17:07:59 +0200

    :sparkles: Add Group/Department Route

--- src/routes/group.route.js
+++ src/routes/group.route.js
@@ -7,10 +7,18 @@ import auth from '../middlewares/auth';
 // Controllers
 import { groupController } from '../controllers/index';
 
-const { createGroup, getAllGroups, getGroup, updateGroup, deleteGroup } =
-  groupController;
+const {
+  createGroup,
+  getAllGroups,
+  getGroup,
+  getGroupsAndDepartments,
+  updateGroup,
+  deleteGroup
+} = groupController;
 
 const router = express.Router();
+
+router.get('/department', getGroupsAndDepartments);
 
 router.route('/').get(getAllGroups).post(auth('admin'), createGroup);
 
