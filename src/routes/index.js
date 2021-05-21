99767a41772d3cfba76a8d93e8af8b55d3183fd1
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-05-21 17:08:13 +0200

    :sparkles: Add Attendance Routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import userRoute from './user.route';
 import groupRoute from './group.route';
 import departmentRoute from './department.route';
 import courseRoute from './course.route';
+import attendanceRoute from './attendance.route';
 
 const router = express.Router();
 
@@ -13,5 +14,6 @@ router.use('/user', userRoute);
 router.use('/group', groupRoute);
 router.use('/department', departmentRoute);
 router.use('/course', courseRoute);
+router.use('/attendance', attendanceRoute);
 
 export default router;
