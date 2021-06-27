ee5076b93e0eea2ccd72cdac2cf0ef78172d01a7
Mahmoud Yasser <mle.mahmoud.yasser@gmail.com>
2021-06-27 21:10:55 +0200

    :sparkles: Update Routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -7,37 +7,17 @@ import { authValidation } from '../validations/index';
 
 const {
   registerValidate,
-  loginValidate,
-  forgotPasswordValidate,
-  resetPasswordValidate,
-  verifyEmailValidate
+  loginValidate
 } = authValidation;
 
 const {
   register,
-  login,
-  forgotPassword,
-  resetPassword,
-  verifyEmail,
-  sendVerificationEmail
+  login
 } = authController;
 
 const router = express.Router();
 
 router.post('/register', validate(registerValidate), register);
 router.post('/login', validate(loginValidate), login);
-router.post(
-  '/forgot-password',
-  validate(forgotPasswordValidate),
-  forgotPassword
-);
-router.post('/reset-password', validate(resetPasswordValidate), resetPassword);
-router.post('/send-verification-email', auth('user'), sendVerificationEmail);
-router.post(
-  '/verify-email',
-  auth('user'),
-  validate(verifyEmailValidate),
-  verifyEmail
-);
 
 export default router;
